5b7496d59e9500a3b9c6cb78c12567c58ff05f38
William <waortiz@hotmail.com>
2018-02-14 11:36:46 -0500

    Fixing unit test.

--- MedicalAttention.Tests/Controllers/AppointmentServiceTest.cs
+++ MedicalAttention.Tests/Controllers/AppointmentServiceTest.cs
@@ -89,9 +89,10 @@ namespace MedicalAttention.Tests.Controllers
 
             mockRepository.Setup(x => x.GetDoctorAppointments(1)).Returns(appointments);
             mockDoctorService.Setup(x => x.GetDoctor(1)).Returns(new Doctor() { Id = 1 });
+            mockPatientService.Setup(x => x.GetPatient(1)).Returns(new Patient() { Id = 1 });
 
             var service = new AppointmentService(mockRepository.Object, mockPatientService.Object, mockDoctorService.Object);
-            service.CreateAppointment(new Appointment() { Id = 3, Date = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, 10, 20, 0), Doctor = new Doctor() { Id = 1 } });
+            service.CreateAppointment(new Appointment() { Id = 3, Date = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day + 1, 10, 20, 0), Doctor = new Doctor() { Id = 1 }, Patient = new Patient() { Id = 1 } });
         }
 
         [TestMethod]
@@ -123,7 +124,7 @@ namespace MedicalAttention.Tests.Controllers
             var service = new AppointmentService(mockRepository.Object, mockPatientService.Object, mockDoctorService.Object);
             service.UpdateAppointment(3, new Appointment() { Id = 3, Date = new DateTime(2017, 3, 2, 3, 20, 0), Doctor = new Doctor() { Id = 1 } });
         }
-        
+
         [TestMethod]
         [ExpectedException(typeof(NotValidDateException), "The minutes of the appointment are not valid.")]
         public void UpdateNotValidAppointment()
@@ -152,7 +153,7 @@ namespace MedicalAttention.Tests.Controllers
             mockDoctorService.Setup(x => x.GetDoctor(1)).Returns(new Doctor() { Id = 1 });
 
             var service = new AppointmentService(mockRepository.Object, mockPatientService.Object, mockDoctorService.Object);
-            service.UpdateAppointment(3, new Appointment() { Id = 3, Date = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, 10, 20, 0), Doctor = new Doctor() { Id = 1 } });
+            service.UpdateAppointment(3, new Appointment() { Id = 3, Date = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day + 1, 10, 20, 0), Doctor = new Doctor() { Id = 1 }, Patient = new Patient() { Id = 1 } });
         }
 
         [TestMethod]
@@ -174,8 +175,8 @@ namespace MedicalAttention.Tests.Controllers
         public void GetDoctorAsignedAppointments()
         {
             var appointments = new List<Appointment>();
-            appointments.Add(new Appointment() { Id = 1, Date = new DateTime(2018, 3, 1, 8, 20, 0), Doctor = new Doctor() { Id = 1 }, State = new State() { Id = (int)StateEnum.Assigned }, Patient = new Patient() { Id= 1 } });
-            appointments.Add(new Appointment() { Id = 2, Date = new DateTime(2018, 3, 2, 8, 20, 0), Doctor = new Doctor() { Id = 1 }, State = new State() { Id = (int)StateEnum.Assigned }, Patient = new Patient() { Id = 2 } } );
+            appointments.Add(new Appointment() { Id = 1, Date = new DateTime(2018, 3, 1, 8, 20, 0), Doctor = new Doctor() { Id = 1 }, State = new State() { Id = (int)StateEnum.Assigned }, Patient = new Patient() { Id = 1 } });
+            appointments.Add(new Appointment() { Id = 2, Date = new DateTime(2018, 3, 2, 8, 20, 0), Doctor = new Doctor() { Id = 1 }, State = new State() { Id = (int)StateEnum.Assigned }, Patient = new Patient() { Id = 2 } });
             appointments.Add(new Appointment() { Id = 3, Date = new DateTime(2018, 3, 2, 10, 20, 0), Doctor = new Doctor() { Id = 1 }, State = new State() { Id = (int)StateEnum.Cancelled }, Patient = new Patient() { Id = 3 } });
 
             mockRepository.Setup(x => x.GetDoctorAppointments(1)).Returns(appointments);
